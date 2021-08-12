e7fcc3bc11a58e32cad6e19efd783f3d91931f9d
Elise Nguyen <eliseqnguyen@Elises-MacBook-Pro.local>
2021-08-12 14:30:28 -0400

    updates to mapcontainer and map

--- card-saver-app/src/MapContainer.jsx
+++ card-saver-app/src/MapContainer.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
+
+const mapStyles = {
+    width: '50%',
+    height: '50%'
+};
 
 export class MapContainer extends Component {
-    render() {
-        return (
-            <p>
-                This is where Map will go in the future
-            </p>
-        );
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
     }
+  };
+
+  render() {
+    return (
+      <CurrentLocation
+        centerAroundCurrentLocation
+        google={this.props.google}
+      >
+        <Marker onClick={this.onMarkerClick} name={'Current Location'} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </CurrentLocation>
+    );
+  }
 }
+
+
+
+export default MapContainer = GoogleApiWrapper({
+        apiKey: 'AIzaSyCL1e2Z_K6Oz2k6_dBSXmPuAarOVOLWCRE'
+    })(MapContainer);
